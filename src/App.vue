a57d63f1e5f8285c7a45aaf87e75ca971ec53ce9
daehyun0 <dd70296@gmail.com>
2022-02-27 04:57:17 +0900

    feat: add SummonerDetailPage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script lang="ts" setup>
 // This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup</script>
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import SummonerDetailPage from "./pages/SummonerDetailPage.vue";</script>
 
-<template></template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <SummonerDetailPage/>
+</template>

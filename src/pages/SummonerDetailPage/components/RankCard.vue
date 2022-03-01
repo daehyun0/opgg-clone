2ce66b838ba9ee912c8b38000ee6f0fc5c66720e
daehyun0 <dd70296@gmail.com>
2022-03-02 07:59:19 +0900

    feat: add RankCard view on hasNotResult state

--- src/pages/SummonerDetailPage/components/RankCard.vue
+++ src/pages/SummonerDetailPage/components/RankCard.vue
@@ -23,21 +23,31 @@ const totalMatchCount = computed(() => props.winCount + props.loseCount);
 </script>
 
 <template>
-  <Card class="rank-card-root" v-if="hasResults">
-    <img :src="tierImageUrl" />
-    <div class="info">
-      <div class="rank-type">{{ rankType }}</div>
-      <div class="most-line-and-total-match-count">
-        <span class="most-line">탑</span>&nbsp;
-        <span class="total-match-count">(총 {{ totalMatchCount }}게임)</span>
+  <Card class="rank-card-root" :class="{unranked: hasResults}" v-if="hasResults">
+    <div class="ranked" v-if="!hasResults">
+      <img :src="tierImageUrl" />
+      <div class="info">
+        <div class="rank-type">{{ rankType }}</div>
+        <div class="most-line-and-total-match-count">
+          <span class="most-line">탑</span>&nbsp;
+          <span class="total-match-count">(총 {{ totalMatchCount }}게임)</span>
+        </div>
+        <div class="tier">{{ tier }} {{ division }}</div>
+        <div class="lp-and-total-match-count">
+          <span class="lp">{{ tierRankPoint }} LP</span>
+          <span> / </span>
+          <span>{{ winCount }}승 {{ loseCount }}패</span>
+        </div>
+        <div class="win-rate">승률 {{ winRateString }}</div>
       </div>
-      <div class="tier">{{ tier }} {{ division }}</div>
-      <div class="lp-and-total-match-count">
-        <span class="lp">{{ tierRankPoint }} LP</span>
-        <span> / </span>
-        <span>{{ winCount }}승 {{ loseCount }}패</span>
+    </div>
+
+    <div class="unranked" v-if="hasResults">
+      <img src="@/assets/unranked.png">
+      <div class="info">
+        <div class="rank-type">{{ rankType }}</div>
+        <div class="unranked">Unranked</div>
       </div>
-      <div class="win-rate">승률 {{ winRateString }}</div>
     </div>
   </Card>
 </template>
@@ -47,69 +57,103 @@ const totalMatchCount = computed(() => props.winCount + props.loseCount);
 @import "@/styles/mixins.scss";
 
 .rank-card-root {
-  @include flexRow(flex-start, center);
-  padding: 9px 7px;
+  & > .ranked {
+    @include flexRow(flex-start, center);
+    padding: 9px 7px;
 
-  & > img {
-    width: 104px;
-    height: 104px;
-  }
-
-  & > .info {
-    & > .rank-type {
-      font-size: 11px;
-      color: v-bind("colors.coolGrey");
+    & > img {
+      width: 104px;
+      height: 104px;
     }
 
-    & > .most-line-and-total-match-count {
-      font-size: 12px;
-      color: #353a3a;
+    & > .info {
+      & > .rank-type {
+        font-size: 11px;
+        color: v-bind("colors.coolGrey");
+      }
 
-      & > .most-line {
+      & > .most-line-and-total-match-count {
+        font-size: 12px;
+        color: #353a3a;
+
+        & > .most-line {
+          font-weight: bold;
+        }
+      }
+
+      & > .rank-type + .most-line-and-total-match-count {
+        margin-top: 4px;
+      }
+
+      & > .tier {
+        margin-top: 4px;
+        font-size: 15px;
         font-weight: bold;
+        color: v-bind("colors.bluish");
+      }
+
+      & > .most-line-and-total-match-count + .tier {
+        margin-top: 4px;
+      }
+
+      & > .lp-and-total-match-count {
+        font-size: 12px;
+        color: v-bind("colors.coolGrey");
+
+        & > .lp {
+          font-weight: bold;
+        }
+      }
+
+      & > .tier + .lp-and-total-match-count {
+        margin-top: 4px;
+      }
+
+      & > .win-rate {
+        font-size: 12px;
+        color: v-bind("colors.coolGrey");
+      }
+
+      & > .lp-and-total-match-count + .win-rate {
+        margin-top: 2px;
       }
     }
 
-    & > .rank-type + .most-line-and-total-match-count {
-      margin-top: 4px;
-    }
-
-    & > .tier {
-      margin-top: 4px;
-      font-size: 15px;
-      font-weight: bold;
-      color: v-bind("colors.bluish");
-    }
-
-    & > .most-line-and-total-match-count + .tier {
-      margin-top: 4px;
-    }
-
-    & > .lp-and-total-match-count {
-      font-size: 12px;
-      color: v-bind("colors.coolGrey");
-
-      & > .lp {
-        font-weight: bold;
-      }
-    }
-
-    & > .tier + .lp-and-total-match-count {
-      margin-top: 4px;
-    }
-
-    & > .win-rate {
-      font-size: 12px;
-      color: v-bind("colors.coolGrey");
-    }
-
-    & > .lp-and-total-match-count + .win-rate {
-      margin-top: 2px;
+    & > img + .info {
+      margin-left: 8px;
     }
   }
 
-  & > img + .info {
-    margin-left: 8px;
+  & > .unranked {
+    @include flexRow(flex-start, center);
+    padding: 16px 27px;
+
+    & > img {
+      width: 64px;
+      height: 64px;
+    }
+
+    & > .info {
+      font-size: 13px;
+      color: v-bind('colors.coolGrey');
+
+      & > .rank-type {
+        font-size: 11px;
+        color: v-bind("colors.coolGrey");
+      }
+
+      & > .unranked {
+        font-weight: bold;
+      }
+
+      & > .rank-type + .unranked {
+        margin-top: 2px;
+      }
+    }
+
+    & > img + .info {
+      margin-left: 28px;
+    }
   }
 }
 </style>

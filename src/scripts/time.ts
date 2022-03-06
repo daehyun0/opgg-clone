c0b58dcc9062766a61d0c06fc9a1c21fc0de8ed2
daehyun0 <dd70296@gmail.com>
2022-03-06 15:59:19 +0900

    fix: edge case on time/getAgoString

--- src/scripts/time.ts
+++ src/scripts/time.ts
@@ -2,7 +2,10 @@ import { DateTime, Duration } from "luxon";
 
 export default {
   getAgoString(seconds: number): string {
-    return DateTime.fromSeconds(seconds).toRelative();
+    return (
+      DateTime.fromSeconds(seconds).toRelative() ||
+      `Wrong Parameter-seconds: ${seconds}`
+    );
   },
   getDurationFromSecond(gameLength: number): string {
     return Duration.fromDurationLike(gameLength * 1000).toFormat("m분 ss초");

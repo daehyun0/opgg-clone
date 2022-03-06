6ded3cf9e21cc75892afdac20407d6e12b28dfc7
daehyun0 <dd70296@gmail.com>
2022-03-06 18:52:59 +0900

    feat: add PreSearchWindow (cannot impelment because api is not serving)

---
+++ src/pages/components/NavigationSection/PreSearchWindow.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onBeforeUnmount, watch, watchEffect } from "vue";
+
+const props = defineProps<{
+  input: string;
+}>();
+
+const stopWatch = watch(
+  () => [props.input],
+  async (newVal, oldVal, onCleanUp) => {
+    const timeoutId = setTimeout(async () => {
+      // const { data } = await opgg.findSummoner(props.input);
+    }, 500);
+
+    onCleanUp(() => clearTimeout(timeoutId));
+  }
+);
+
+onBeforeUnmount(() => {
+  stopWatch();
+});
+</script>
+
+<template>
+  <div class="history-window-root"></div>
+</template>
+
+<style scoped></style>

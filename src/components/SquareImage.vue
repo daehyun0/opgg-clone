70fee35186fe0655278c83425953a8edba4c3ab6
daehyun0 <dd70296@gmail.com>
2022-03-06 15:01:01 +0900

    feat: add SquareImage for common components

---
+++ src/components/SquareImage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  imageUrl: string;
+  size: number;
+}>();
+</script>
+
+<template>
+  <img
+      :src="imageUrl"
+      :style="{ width: size + 'px', height: size + 'px' }"
+  />
+</template>
+
+<style scoped>
+
+</style>
